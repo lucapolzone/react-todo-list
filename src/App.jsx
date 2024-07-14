831608a3aeeac9cd8b2e21a70b837e9edbe7801b
lucapolzone <lucapolzone@gmail.com>
2024-07-14 16:08:50 +0200

    reset again vite scaffolding files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import { useState } from 'react'
 import './style.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-    </>
-  )
-}
 
 export default function App(){
-  
+
 };
